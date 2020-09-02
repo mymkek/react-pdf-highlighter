14edf23b5383074a0a0c5fec78000bf9f1d94de3
mymkek <35272185+mymkek@users.noreply.github.com>
2020-09-02 17:42:32 +0300

    rename files

---
+++ lib/components/AreaHighlight.jsx
@@ -0,0 +1,55 @@
+//      
+
+import React, {Component} from "react";
+
+// $FlowFixMe
+import Rnd from "react-rnd";
+
+import "../style/AreaHighlight.css";
+
+
+class AreaHighlight extends Component {
+    render() {
+        const {highlight, onChange, ...otherProps} = this.props;
+
+        return (
+            <Rnd
+                className="AreaHighlight"
+                onDragStop={(_, data) => {
+                    const boundingRect = {
+                        ...highlight.position.boundingRect,
+                        top: data.y,
+                        left: data.x
+                    };
+
+                    onChange(boundingRect);
+                }}
+                onResizeStop={(_, direction, ref, delta, position) => {
+                    const boundingRect = {
+                        top: position.y,
+                        left: position.x,
+                        width: ref.offsetWidth,
+                        height: ref.offsetHeight
+                    };
+
+                    onChange(boundingRect);
+                }}
+                position={{
+                    x: highlight.position.boundingRect.left,
+                    y: highlight.position.boundingRect.top
+                }}
+                size={{
+                    width: highlight.position.boundingRect.width,
+                    height: highlight.position.boundingRect.height
+                }}
+                onClick={event => {
+                    event.stopPropagation();
+                    event.preventDefault();
+                }}
+                {...otherProps}
+            />
+        );
+    }
+}
+
+export default AreaHighlight;
